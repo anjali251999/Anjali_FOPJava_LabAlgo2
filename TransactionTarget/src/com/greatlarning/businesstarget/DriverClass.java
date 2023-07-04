a279ae94cbdcab1fec973834b020efdd72028528
anjali251999 <132471579+anjali251999@users.noreply.github.com>
2023-07-04 21:26:28 +0530

    Add files via upload

---
+++ TransactionTarget/src/com/greatlarning/businesstarget/DriverClass.java
@@ -0,0 +1,29 @@
+package com.greatlarning.businesstarget;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class DriverClass {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter the number of transactional days:");
+		int nod = sc.nextInt();
+
+		int earnings[] = new int[nod]; // Dynamic Array
+		System.out.println("Enter the transcations for " + nod + " days");
+		for (int i = 0; i < earnings.length; i++) {
+			earnings[i] = sc.nextInt();
+		}
+
+		System.out.println(Arrays.toString(earnings));
+
+		System.out.println("Enter the number of targets:");
+		int targets = sc.nextInt();
+
+		Target tc = new Target();
+		tc.targetCheck(earnings, targets);
+	}
+
+}
